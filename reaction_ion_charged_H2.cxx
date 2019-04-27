d8618c938cefc49fa4d0afb734740b72e0de01a0
yz3138 <46000195+yz3138@users.noreply.github.com>
2019-04-27 17:04:20 +0100

    Update reaction_ion_charged_H2.cxx

--- reaction_ion_charged_H2.cxx
+++ reaction_ion_charged_H2.cxx
@@ -18,7 +18,7 @@ public:
     bool diagnose;
     OPTION(options, diagnose, false);
     if (diagnose) {
-      SAVE_REPEAT4(Riz, Eiz, Fiz, Siz);
+      SAVE_REPEAT4(Riz_2, Eiz_2, Fiz_2, Siz_2);
     }
   }
 
@@ -47,13 +47,13 @@ public:
  
     Coordinates *coord = bout::globals::mesh->getCoordinates();
 
-    Riz = 0.0;
-    Eiz = 0.0;
-    Fiz = 0.0;
-    Siz = 0.0;
+    Riz_2 = 0.0;
+    Eiz_2 = 0.0;
+    Fiz_2 = 0.0;
+    Siz_2 = 0.0;
     
     CELL_AVERAGE(i,                        // Index variable
-                 Riz.getRegion(RGN_NOBNDRY),  // Index and region (input)
+                 Riz_2.getRegion(RGN_NOBNDRY),  // Index and region (input)
                  coord,                    // Coordinate system (input)
                  weight,                   // Quadrature weight variable
                  Ne, Nm_c, Te, Tm_c, Vm_c) {     // Field variables
@@ -62,38 +62,38 @@ public:
         Ne * Nm_c * hydrogen.Ion_charged_H2(Te*Tnorm) * (Nnorm / Omega_ci);
 
       // Electron energy loss per ionisation
-      Riz[i] += weight * (Ediss / Tnorm) * R;
+      Riz_2[i] += weight * (Ediss / Tnorm) * R;
 
       // Energy from neutral atom temperature
-      Eiz[i] -= weight * (3. / 2) * Tm_c * R;
+      Eiz_2[i] -= weight * (3. / 2) * Tm_c * R;
 
       // Friction due to ionisation
-      Fiz[i] -= weight * Vm_c * R;
+      Fiz_2[i] -= weight * Vm_c * R;
 
       // Plasma sink due to ionisation (negative)
-      Siz[i] -= weight * R;
+      Siz_2[i] -= weight * R;
     }
   }
   
   SourceMap densitySources() override {
-    return {{"h2+", Siz},  // Siz < 0 => H2+ source 
-            {"h+", -2*Siz}};   // Siz < 0 => H+ sink
+    return {{"h2+", Siz_2},  // Siz < 0 => H2+ source 
+            {"h+", -2*Siz_2}};   // Siz < 0 => H+ sink
   }
   SourceMap momentumSources() {
-    return {{"h2+",Fiz},
-            {"h+",-0.5*Fiz}};  
+    return {{"h2+",Fiz_2},
+            {"h+",-0.5*Fiz_2}};  
   }
   SourceMap energySources() {
-    return {{"h2+", Eiz}, // Eiz < 0 => Ion energy source
-            {"e", -Riz},  // Electron energy into ionisation
-            {"h+", -0.5*Eiz}};  // 
+    return {{"h2+", Eiz_2}, // Eiz < 0 => Ion energy source
+            {"e", -Riz_2},  // Electron energy into ionisation
+            {"h+", -0.5*Eiz_2}};  // 
   }
   
   std::string str() const { return "charged molecule dissociation"; }
   
 private:
   UpdatedRadiatedPower hydrogen; // Atomic rates
-  Field3D Riz, Eiz, Fiz, Siz;
+  Field3D Riz_2, Eiz_2, Fiz_2, Siz_2;
 
   BoutReal Ediss;  // Ionisation energy loss [eV]
 };

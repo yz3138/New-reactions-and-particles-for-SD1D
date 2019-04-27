e0e5fb7f82415c7b2d70c25ce9a06dea62fb5484
yz3138 <46000195+yz3138@users.noreply.github.com>
2019-04-27 17:00:53 +0100

    Update reaction_dissociation_between_H_H2.cxx

--- reaction_dissociation_between_H_H2.cxx
+++ reaction_dissociation_between_H_H2.cxx
@@ -24,7 +24,7 @@ public:
     bool diagnose;
     OPTION(options, diagnose, false);
     if (diagnose) {
-      SAVE_REPEAT(Sn,En,Em,Fn);
+      SAVE_REPEAT(Sn_1,En_1,Em_1,Fn_1);
     }
   }
 
@@ -42,13 +42,13 @@ public:
     Field3D Nn{atoms.N}, Tn{atoms.T}, Vn{atoms.V};
     Coordinates *coord = mesh->getCoordinates();
 
-    Sn = 0.0;
-    En = 0.0;
-    Fn = 0.0;
-    Em = 0.0;
+    Sn_1 = 0.0;
+    En_1 = 0.0;
+    Fn_1 = 0.0;
+    Em_1 = 0.0;
 //    Sn_1=0.0;
     CELL_AVERAGE(i,                          // Index variable
-                 Sn.getRegion(RGN_NOBNDRY), // Index and region (input)
+                 Sn_1.getRegion(RGN_NOBNDRY), // Index and region (input)
                  //Sn_1.getRegion(RGN_NOBNDRY),
                  coord,                      // Coordinate system (input)
                  weight,                     // Quadrature weight variable
@@ -61,31 +61,31 @@ public:
                             // ~~~~~  Rate here in SI units m^3/s
 
       // Ecx is energy transferred from ions to neutrals
-      Sn[i] += weight * R;  //Source term from DI and Diss_H_H2
+      Sn_1[i] += weight * R;  //Source term from DI and Diss_H_H2
 //      Sn_1[i] += weight * R1;
-      En[i] -= weight * (Ediss/Tnorm) * R;
-      Em[i] -= weight * (3./2.) * Tm * R; // energy transferred from molecule to atom
-      Fn[i] -= weight * (Vn-Vm) * R;
+      En_1[i] -= weight * (Ediss/Tnorm) * R;
+      Em_1[i] -= weight * (3./2.) * Tm * R; // energy transferred from molecule to atom
+      Fn_1[i] -= weight * (Vn-Vm) * R;
  
     }
   }
   SourceMap densitySources() override {
-    return {{"h", Sn},   ///????  why is not '2*Sn' for h, and 'Sn' for h2????
-            {"h2", -0.5*Sn}}; // Redistribute neutrals
+    return {{"h", Sn_1},   ///????  why is not '2*Sn' for h, and 'Sn' for h2????
+            {"h2", -0.5*Sn_1}}; // Redistribute neutrals
   }
   SourceMap momentumSources() {
-    return {{"h", Fn},
-            {"h2",-Fn}};
+    return {{"h", Fn_1},
+            {"h2",-Fn_1}};
   }
   SourceMap energySources() {
-    return {{"h", En-0.5*Em},   ///  H2->H+H, so the energy also splitted by 2
-            {"h2",Em}};
+    return {{"h", En_1-0.5*Em_1},   ///  H2->H+H, so the energy also splitted by 2
+            {"h2",Em_1}};
   }
 
   std::string str() const { return "Hydrogen dissociation_DissH"; }
 
 private:
-  Field3D Sn,En,Fn,Em; // Source of atoms
+  Field3D Sn_1,En_1,Fn_1,Em_1; // Source of atoms
   UpdatedRadiatedPower hydrogen; // Atomic rates
   BoutReal Ediss;
 };
